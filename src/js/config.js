5a45aceff5900926845a8cc1299b8acc68383b89
ShrineOX <thitheluc.la@gmail.com>
2021-04-09 23:16:58 +0700

    Changed API key

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = '3e57348f-7aa9-4456-82e2-6a950ddd2b1b';
 export const MODAL_CLOSE_SEC = 2.5;
